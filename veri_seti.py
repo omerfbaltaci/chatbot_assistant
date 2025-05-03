fa0a7bf8400726d7f7e1988b5b1acafdb8409761
Ömer Faruk Baltacı <baltaci523@gmail.com>
2025-05-04 01:10:11 +0300

    First commit
    In this version of the chatbot assistant, the model can only find the necessary answer and print it on the screen when asked questions that are very similar to the questions in the given dataset. If the question expression entered by the user is very different from the question examples in the dataset, the model does not work consistently.

---
+++ veri_seti.py
@@ -0,0 +1,6 @@
+veri_seti = [
+    {"id": 1, "question": "Akademide hangi gün gelmem gerekiyor?", "answer": "Hafta içi her gün 10:00-17:00 saatleri arasında bireysel çalışma için akademide bulunabilirsin."},
+    {"id": 2, "question": "Sigorta işlemlerimi nereden öğrenebilirim?", "answer": "Sigorta işlemleri için Acunmedya İnsan Kaynakları birimiyle iletişime geçmelisin."},
+    {"id": 3, "question": "Hocalarım kimler?", "answer": "Hocaların ders içeriklerine göre değişiyor. Güncel listeyi Akademi Koordinatörü'nden öğrenebilirsin."},
+     {"id": 4, "question": "alihan", "answer": "dopamin"}
+]
